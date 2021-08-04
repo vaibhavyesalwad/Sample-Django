65461e04da9ac281bfeb3bb7a03996337b1a30aa
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-04 19:12:19 +0530

    Create OrderItem model

--- store/models.py
+++ store/models.py
@@ -55,6 +55,13 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
