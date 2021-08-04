df2ee5933fd0ba4bfa6ce7b36c8565ed21df31c2
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-04 18:28:35 +0530

    Create choice field- membership

--- store/models.py
+++ store/models.py
@@ -12,8 +12,20 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = "B"
+    MEMBERSHIP_SILVER = "S"
+    MEMBERSHIP_GOLD = "G"
+
+    MEMBERSHIP_CHOICES = [
+        ("B", "Bronze"),
+        ("G", "Gold"),
+        ("S", "Silver")
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
