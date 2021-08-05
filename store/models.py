7f080508a62f092269476dbb4516813ae4f0c3ef
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-05 13:38:48 +0530

    Add slug field to product

--- store/models.py
+++ store/models.py
@@ -18,6 +18,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)  # varchar(255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
