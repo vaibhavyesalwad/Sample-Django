9a3b1a1b719c509afb951ca7fefa1552af015bc9
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-13 18:31:15 +0530

    Call database functions

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,20 @@
 from django.db.models.aggregates import Count, Max, Min
+from django.db.models.expressions import Func
 from django.shortcuts import render
-from django.db.models import Q, F, Value
+from django.db.models import Q, F, Value, Func
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Min, Max, Sum, Count, Avg
-from store.models import Order, OrderItem, Product
+from store.models import CartItem, Customer, Order, OrderItem, Product
 
 
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = Product.objects.annotate(new_id=F('id') + 2)
+    query_set = Customer.objects.annotate(full_name=Func(
+        F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
+
+    # query_set = Customer.objects.annotate(
+    #     full_name=Concat('first_name', Value(' '), 'last_name'))
 
     return render(request, 'hello.html', {"name": "Vaibhav", "query_set": list(query_set)})
