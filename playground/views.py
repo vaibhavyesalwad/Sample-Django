c00fcd03daa3543ca278c3391130ba39b0433108
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-13 17:17:21 +0530

    Selecting related fields

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import OrderItem, Product
+from store.models import Order, OrderItem, Product
 
 
 # Create your views here.
 
 
 def say_hello(request):
-    products = Product.objects.defer('title')
-    # print(products)
+    orders = Order.objects.order_by(
+        '-placed_at').select_related('customer').prefetch_related('orderitem_set__product')[:5]
 
-    # products = Product.objects.only('id')
-    print(products)
-
-    return render(request, 'hello.html', {"name": "Vaibhav", "products": list(products)})
+    return render(request, 'hello.html', {"name": "Vaibhav", "orders": list(orders)})
