95620c81e20d0d900ce0aa6222da5a5fccd7b666
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-13 18:55:19 +0530

    Using expression wrappers

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,8 @@
 from django.db.models.aggregates import Count, Max, Min
-from django.db.models.expressions import Func
+from django.db.models.expressions import ExpressionWrapper, Func
+from django.db.models.fields import DecimalField
 from django.shortcuts import render
-from django.db.models import Q, F, Value, Func
+from django.db.models import Q, F, Value, Func, ExpressionWrapper
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Min, Max, Sum, Count, Avg
 from store.models import CartItem, Customer, Order, OrderItem, Product
@@ -11,6 +12,8 @@ from store.models import CartItem, Customer, Order, OrderItem, Product
 
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(orders_count=Count('order'))
+    discounted_price = ExpressionWrapper(
+        F('unit_price')*0.8, output_field=DecimalField())
+    query_set = Product.objects.annotate(discounted_price=discounted_price)
 
     return render(request, 'hello.html', {"name": "Vaibhav", "query_set": list(query_set)})
