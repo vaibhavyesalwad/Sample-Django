eab656bbbfbc94dcd1ccbe6d9d509949d7fccd17
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-14 12:12:06 +0530

    Insert data using objects (Manager)

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from django.db.models.expressions import ExpressionWrapper, Func
 from django.db.models.fields import DecimalField
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
-from store.models import Product
+from store.models import Collection, Product
 from tags.models import TaggedItem
 
 
@@ -11,14 +11,9 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
+    collection = Collection()
+    collection.title = 'Video Games'
+    collection.featured_product = Product(pk=1)
+    collection.save()
 
-    # query is cached only if evaluate whole queryset
-    query_set[0]        # Now 2 queries 1. first row
-    list(query_set)     # 2. all rows
-
-    # list(query_set)      # Now only one query all rows
-    # query_set[0]         # first row from cached queryset
-
-    # reads already cached queryset
-    return render(request, 'hello.html', {"name": "Vaibhav", "result": list(query_set)})
+    return render(request, 'hello.html', {"name": "Vaibhav"})
