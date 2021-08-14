6eaf803aa1f91b33336306c39fdac98e44a468c2
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-14 11:51:04 +0530

    Using custom manager

--- playground/views.py
+++ playground/views.py
@@ -11,12 +11,6 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    content_type = ContentType.objects.get_for_model(Product)
-
-    # finding tags for product with id = 1
-    query_set = TaggedItem.objects.select_related('tag').filter(
-        content_type=content_type,
-        object_id=1
-    )
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
 
     return render(request, 'hello.html', {"name": "Vaibhav", "result": list(query_set)})
