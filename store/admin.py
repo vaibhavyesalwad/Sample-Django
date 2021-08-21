4be9aaaefa34bcaa1c7b539aff65e35bf5f34707
Vaibhav Yesalwad <vaibhavyesalwad@gmail.com>
2021-08-21 19:24:27 +0530

    Edit children using inlines

--- store/admin.py
+++ store/admin.py
@@ -13,8 +13,8 @@ from . import models
 
 
 class InventoryFilter(admin.SimpleListFilter):
-    title = 'inventory'
     parameter_name = 'inventory'
+    title = 'inventory'
 
     def lookups(self, request, model_admin):
         return [
@@ -30,8 +30,8 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    search_fields = ['title']
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count')
     def products_count(self, collection):
@@ -50,18 +50,17 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['collection']
-    prepopulated_fields = {
-        'slug': ['title']
-    }
     actions = ['clear_inventory']
+    autocomplete_fields = ['collection']
     list_display = ['title', 'unit_price',
                     'inventory_status', 'collection_title']
     list_editable = ['unit_price']
-    ordering = ['title']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 20
     list_select_related = ['collection']
-    list_filter = ['collection', 'last_update', InventoryFilter]
+    ordering = ['title']
+    prepopulated_fields = {'slug': ['title']}
+    search_fields = ['title']
 
     def collection_title(self, product):
         return product.collection.title
@@ -86,8 +85,8 @@ class ProductAdmin(admin.ModelAdmin):
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 20
+    ordering = ['first_name', 'last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
 
     @admin.display(ordering='orders_count')
@@ -105,7 +104,16 @@ class CustomerAdmin(admin.ModelAdmin):
         )
 
 
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    extra = 1
+    max_num = 10
+    min_num = 1
+    model = models.OrderItem
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'placed_at', 'customer']
     autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
+    list_display = ['id', 'placed_at', 'customer']
